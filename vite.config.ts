3a10d1ead0f92b8b0aca8c482e178478fae76055
Andy.Z <hzzhengyan@qq.com>
2026-02-13 11:05:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Bio-Crisis-Zero-Hour/',   // 注意大小写，要和仓库名一致
       server: {
         port: 3000,
         host: '0.0.0.0',
